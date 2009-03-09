19091cf201bd98ab4a82d801899ba20ee7bc77a0
Anthony Short <anthony@anthonyshort.com.au>
2009-03-09 16:14:32 +1100

    Changed the way the options for plugins are handled

--- system/config.php
+++ system/config.php
@@ -45,31 +45,47 @@ $path = array(
 	"image_titles" 	=> "assets/titles"
 );
 
-// CSSTidy Options
-$tidy_options = array(
-	'preserve_css' 				=> false,
-	'sort_selectors' 			=> false,
-	'sort_properties' 			=> true,
-	'merge_selectors' 			=> 2,
-	'optimise_shorthands'		=> 1,
-	'compress_colors' 			=> true,
-	'compress_font-weight' 		=> false,
-	'lowercase_s' 				=> true,
-	'case_properties' 			=> 1,
-	'remove_bslash' 			=> false,
-	'remove_last_;' 			=> true,
-	'discard_invalid_properties' => false,
-	'css_level' 				=> 'CSS2.1',
-	'timestamp' 				=> false,
-	'template'					=> 'highest_compression'
+$options = array(
+
+	// CSSTidy options
+	'csstidy' => array(
+		'preserve_css' 				=> false,
+		'sort_selectors' 			=> false,
+		'sort_properties' 			=> true,
+		'merge_selectors' 			=> 2,
+		'optimise_shorthands'		=> 1,
+		'compress_colors' 			=> true,
+		'compress_font-weight' 		=> false,
+		'lowercase_s' 				=> true,
+		'case_properties' 			=> 1,
+		'remove_bslash' 			=> false,
+		'remove_last_;' 			=> true,
+		'discard_invalid_properties' => false,
+		'css_level' 				=> 'CSS2.1',
+		'timestamp' 				=> false,
+		'template'					=> 'highest_compression'
+	),
+	
+	// Grid layout options
+	'grid_layout' => array(
+		'create_grid_css' => true,
+		'push' => true,
+		'pull' => true,
+		'append' => true,
+		'prepend' => true,
+		'columns-x' => true,
+		'baseline-x' => true,
+		'baseline-push-x' => true,
+		'baseline-pull-x' => true
+	)
+	
 );
 
 // Control the order the plugins are loaded. 
 // Comment out plugins you don't want to load.
 $plugin_order = array(
 	'ServerImportPlugin',
-	'Browsers'	,
-	'Append',
+	'Browsers',
 	'BasedOnPlugin',
 	'NestedSelectorsPlugin',
 	'ConstantsPlugin',
@@ -78,10 +94,13 @@ $plugin_order = array(
 	'ImageReplacement',
 	'AddTo',
 	'Base64Plugin',
-	'CSSTidyPlugin',
+	//'CSSTidyPlugin',
 	'CSS3Helper',
+	'Append',
 	'CondenserPlugin',
 	//'PrettyPlugin'
 );
+
+
 
 ?>
