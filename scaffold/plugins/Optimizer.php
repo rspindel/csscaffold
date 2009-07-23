bfb267da2f5fdbb8f6338ec120ebfd21640e247d
Anthony Short <anthony@anthonyshort.com.au>
2009-07-24 00:21:12 +1000

    CSSTidy/Optimizer now only fires when you include tidy in the url string like so: http://scaffold/scaffold/css/screen.css&tidy. Tidy can have negative effects on speed and may sometimes break your css if you're not careful, so this means CSSTidy is now off by default.

--- scaffold/plugins/Optimizer.php
+++ scaffold/plugins/Optimizer.php
@@ -11,31 +11,31 @@ require join_path(BASEPATH,'libraries/CSSTidy.php');
 class Optimizer extends Plugins
 {
 	function formatting_process()
-	{				
-		$tidy = new csstidy();
-							
-		$tidy->set_cfg('preserve_css',false);
-		$tidy->set_cfg('sort_selectors',false);
-		$tidy->set_cfg('sort_properties',true);
-		$tidy->set_cfg('merge_selectors',2);
-		$tidy->set_cfg('optimise_shorthands',1);
-		$tidy->set_cfg('compress_colors',true);
-		$tidy->set_cfg('compress_font-weight',false);
-		$tidy->set_cfg('lowercase_s',true);
-		$tidy->set_cfg('case_properties',1);
-		$tidy->set_cfg('remove_bslash',false);
-		$tidy->set_cfg('remove_last_;',true);
-		$tidy->set_cfg('discard_invalid_properties',false);
-		$tidy->set_cfg('css_level','CSS2.1');
-		$tidy->set_cfg('time_stamp','false');
-		
-		$tidy->load_template('highest_compression');
-		
-		$result = $tidy->parse(CSS::$css);
-				
-		CSS::$css = $tidy->print->plain();  
+	{
+		if(Config::get('tidy') === true)
+		{
+			$tidy = new csstidy();
+								
+			$tidy->set_cfg('preserve_css',false);
+			$tidy->set_cfg('sort_selectors',false);
+			$tidy->set_cfg('sort_properties',true);
+			$tidy->set_cfg('merge_selectors',2);
+			$tidy->set_cfg('optimise_shorthands',1);
+			$tidy->set_cfg('compress_colors',true);
+			$tidy->set_cfg('compress_font-weight',false);
+			$tidy->set_cfg('lowercase_s',true);
+			$tidy->set_cfg('case_properties',1);
+			$tidy->set_cfg('remove_bslash',false);
+			$tidy->set_cfg('remove_last_;',true);
+			$tidy->set_cfg('discard_invalid_properties',false);
+			$tidy->set_cfg('css_level','CSS2.1');
+			$tidy->set_cfg('time_stamp','false');
+			
+			$tidy->load_template('highest_compression');
+			
+			$result = $tidy->parse(CSS::$css);
+					
+			CSS::$css = $tidy->print->plain(); 
+		}
 	}
 } 
-
-
-?>
