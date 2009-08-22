132bef8620b67c90edac034f56d59423c4d396d8
Anthony Short <anthonyshort@anthony-shorts-macbook-pro.local>
2009-08-22 10:51:07 +1000

    Cleaned up the config file.

--- scaffold/config.php
+++ scaffold/config.php
@@ -1,43 +1,29 @@
 <?php
 
-/******************************************************************************
-* System Configuration
- ******************************************************************************/
-	
-	/**
-	 * CACHE LOCK
-	 *
-	 * If you lock the cache, it will never recache your css
-	 *
-	 * @var boolean
-	 **/	
-	$config['cache_lock'] = false;
-	
-	/**
-	 * ALWAYS RECACHE
-	 *
-	 * If true, it will recache the css every time. This means
-	 * you don't need to do ?recache during development.
-	 *
-	 * @var boolean
-	 **/	
-	$config['always_recache'] = false;
-	
-	/**
-	 * SHOW CSS HEADER INFORMATION
-	 *
-	 * Output information at the top of your cached file for debugging
-	 *
-	 * @var boolean
-	 **/
-	$config['show_header'] = true;
-	
-	/**
-	* DEBUG MODE
-	*
-	* Enables FirePHP for debugging. DO NOT HAVE THIS TURNED ON WHEN
-	* YOUR SITE IS LIVE! IT COULD DISPLAY DANGEROUS SYSTEM INFORMATION
-	*
-	* @var boolean
-	**/
-	$config['debug'] = false;
+/**
+ * CACHE LOCK
+ *
+ * If you lock the cache, it will never recache your css
+ *
+ * @var boolean
+ **/	
+$config['cache_lock'] = false;
+
+/**
+ * ALWAYS RECACHE
+ *
+ * If true, it will recache the css every time. This means
+ * you don't need to do ?recache during development.
+ *
+ * @var boolean
+ **/	
+$config['always_recache'] = true;
+
+/**
+ * SHOW CSS HEADER INFORMATION
+ *
+ * Output information at the top of your cached file for debugging
+ *
+ * @var boolean
+ **/
+$config['show_header'] = true;
