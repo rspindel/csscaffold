5e877f360434e9f8e6d93b3f00a6c5dffde6565b
Anthony Short <anthonyshort@me.com>
2010-01-31 20:30:35 +1100

    Fixed the url_path function

--- scaffold/libraries/Scaffold/Utils.php
+++ scaffold/libraries/Scaffold/Utils.php
@@ -18,7 +18,7 @@ class Scaffold_Utils
 	 */
 	public static function url_path($path) 
 	{
-		return self::reduce_double_slashes(str_replace( SCAFFOLD_SYSPATH, SCAFFOLD_URLPATH, realpath($path) ));
+		return self::reduce_double_slashes(str_replace( SCAFFOLD_DOCROOT, '/', realpath($path) ));
 	}
 
 	/**
